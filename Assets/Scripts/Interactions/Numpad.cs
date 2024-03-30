4165d173e153ab9a06d363351cc4166cce7f5b79
KSalmaze <pedrohsalmaze@gmail.com>
2024-03-30 16:24:04 -0300

    Bug da luz do reator resolvido

--- Assets/Scripts/Interactions/Numpad.cs
+++ Assets/Scripts/Interactions/Numpad.cs
@@ -7,14 +7,26 @@ public class Numpad : Interaction
     [SerializeField] private GameObject lampadaA;
     [SerializeField] private GameObject lampadaB;
     [SerializeField] private GameObject gira;
+    [SerializeField] private Color newColor;
+    [SerializeField] private float newRotation;
     public override void Interact()
+    {
+        CorrectPassword();
+    }
+
+    private void CorrectPassword()
     {
         Debug.Log("Numpad foi");
         PlayerPrefs.SetInt("Reator",1);
-
-        gira.GetComponent<Spin>().rotationSpeed = 125;
-        lampadaA.GetComponent<Light>().color = Color.blue;
-        lampadaB.GetComponent<Light>().color = Color.blue;
-        //#008000
+        
+        gira.GetComponent<Spin>().rotationSpeed = newRotation;
+        ChangeLightColor();
+    }
+    
+    
+    private void ChangeLightColor()
+    {
+        lampadaA.GetComponent<Light>().color = newColor;
+        lampadaB.GetComponent<Light>().color = newColor;
     }
 }
